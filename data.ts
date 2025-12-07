54f839930bb56d94d08af5b565c090d4320b9ac3
mai-kuraki <1530850503@qq.com>
2025-12-07 17:09:57 +0800

    refactor: Remove unused static data
    The `data.ts` file contained static question data which is no longer relevant as questions are now fetched dynamically. This commit removes the unused file to clean up the codebase.

--- data.ts
+++
@@ -1,5 +0,0 @@
-
-// This file is no longer used as data is fetched dynamically.
-// Keeping it to prevent import errors if referenced elsewhere.
-import { Question } from './types';
-export const DATA: Question[] = [];
